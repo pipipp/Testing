d7c444d5ea28f2b0d126d36280238287094a0ad0
evaliu <1872863416@qq.com>
2020-12-05 16:24:26 +0800

    update

--- unit_testing/template/run.py
+++ unit_testing/template/run.py
@@ -11,14 +11,14 @@ def run():
     # 方案一
     unittest.main()  # 直接调用main方法启动所有以"test_"开头命名的方法，用例按照字母顺序执行
 
-    # 方案二（调用unittest类方法，自定义添加用例和执行顺序）
+    # 方案二（使用suite套件，调用unittest类方法，自定义添加用例和执行顺序）
     suite = unittest.TestSuite()  # 实例化测试套件
     for each in ['test_case_3', 'test_case_1', 'test_case_2']:  # 按照加载顺序执行用例
         suite.addTest(UnitTestExample(each))
     runner = unittest.TextTestRunner()
     runner.run(suite)
 
-    # 方案三（使用加载python文件的方式启动所有用例）
+    # 方案三（使用discover测试集，加载python文件启动所有用例）
     test_dir = os.path.join(os.path.dirname(__file__), 'case')  # 用例所在目录
     execute_python_file = 'test_*.py'  # 启动指定命名规则的python文件
     discover = unittest.defaultTestLoader.discover(start_dir=test_dir, pattern=execute_python_file)
