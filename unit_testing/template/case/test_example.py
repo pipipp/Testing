2615fe6ca285b2ac98857e70986565d527c15f80
evaliu <1872863416@qq.com>
2020-12-05 16:42:01 +0800

    update

--- unit_testing/template/case/test_example.py
+++ unit_testing/template/case/test_example.py
@@ -1,10 +1,16 @@
 """
 unittest常用断言方法：
-1. assertEqual(a, b, msg="Error info") 判断a,b是否相等，不相等时，抛出 msg
-2. assertTure(x, msg="Error info") 判断x表达式是否为真，表达式为假时，抛出 msg
-3. assertIn(a, b, msg="Error info") 判断a是否在b里面，a不在b里面时，抛出 msg
-4. assertIsNone(x, msg="Error info") 判断x是否为空，x不为空时，抛出 msg
+1. assertEqual(a, b, msg="Error info")      # 判断a,b是否相等，不相等时，抛出 msg
+2. assertTure(x, msg="Error info")          # 判断x表达式是否为真，表达式为假时，抛出 msg
+3. assertIn(a, b, msg="Error info")         # 判断a是否在b里面，a不在b里面时，抛出 msg
+4. assertIsNone(x, msg="Error info")        # 判断x是否为空，x不为空时，抛出 msg
+
+跳过用例方法：使用装饰器
+@unittest.skip(reason)                       # 直接指定skip，reason用于描述跳过的原因
+@unittest.skipIf(condition, reason)          # 当condition为True时，跳过用例
+@unittest.skipUnless(condition, reason)      # 当condition为True时，执行用例
 """
+import sys
 import unittest
 
 __author__ = 'Evan'
@@ -43,7 +49,8 @@ class UnitTestExample(unittest.TestCase):
         with self.assertRaises(TypeError):
             self._add(1, [2])
 
-    @unittest.skip('skip the case test for test_case_2')
+    # 跳过指定的用例
+    @unittest.skip('Use the "skip" method to skip the case test for test_case_2')
     def test_case_2(self, a=1, b=2, expect_value=30):
         """
         计算两个值相加，如果测试值和期待值相等就为Pass
@@ -55,6 +62,9 @@ class UnitTestExample(unittest.TestCase):
         result = self._add(a, b)
         self.assertEqual(result, expect_value, msg=f'Failed message: {a} + {b} != {expect_value}')
 
+    # 跳过条件不为True的用例
+    @unittest.skipUnless(sys.platform.startswith('win'),
+                         'Use the "skipUnless" method to skip tests other than on Windows platforms')
     def test_case_3(self, a=7, b=2, expect_value=50):
         """
         计算两个值相减，如果测试值和期待值相等就为Pass
